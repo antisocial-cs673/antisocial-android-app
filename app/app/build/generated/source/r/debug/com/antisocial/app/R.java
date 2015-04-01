32b5834bac5f4aa9f0e41ee2f8be8d3db6afb226
Igibek Koishybayev <koishybayev.igibek@gmail.com>
2015-04-01 16:45:32 -0400

    Revert "Merged sound feature"
    This reverts commit 105ccc2ac25304106a45a627471019916b0cff1e.

--- app/app/build/generated/source/r/debug/com/antisocial/app/R.java
+++ app/app/build/generated/source/r/debug/com/antisocial/app/R.java
@@ -11,16 +11,16 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int black=0x7f050000;
-        public static final int light_blue=0x7f050001;
-        public static final int light_grey=0x7f050002;
-        public static final int listview_bg_sel=0x7f050003;
-        public static final int transparent=0x7f050004;
-        public static final int white=0x7f050005;
+        public static final int black=0x7f040000;
+        public static final int light_blue=0x7f040001;
+        public static final int light_grey=0x7f040002;
+        public static final int listview_bg_sel=0x7f040003;
+        public static final int transparent=0x7f040004;
+        public static final int white=0x7f040005;
     }
     public static final class dimen {
-        public static final int activity_horizontal_margin=0x7f060000;
-        public static final int activity_vertical_margin=0x7f060001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int abs__spinner_48_inner_holo=0x7f020000;
@@ -44,32 +44,32 @@ public final class R {
         public static final int whatsapp_trans=0x7f020012;
     }
     public static final class id {
-        public static final int action_settings=0x7f0a0019;
-        public static final int app_logo=0x7f0a0015;
-        public static final int app_name=0x7f0a0016;
-        public static final int begin=0x7f0a0001;
-        public static final int block_item=0x7f0a0018;
-        public static final int button=0x7f0a000a;
-        public static final int check_box=0x7f0a0017;
-        public static final int container=0x7f0a0007;
-        public static final int facebookView=0x7f0a0003;
-        public static final int listViewLayout=0x7f0a0014;
-        public static final int progressBar=0x7f0a0002;
-        public static final int progressView1=0x7f0a0000;
-        public static final int section_label=0x7f0a0011;
-        public static final int set_block_list=0x7f0a0013;
-        public static final int start_block_btn=0x7f0a0012;
-        public static final int statusTextView=0x7f0a0008;
-        public static final int textView=0x7f0a0009;
-        public static final int textView0=0x7f0a000b;
-        public static final int textView1=0x7f0a000c;
-        public static final int textView2=0x7f0a000d;
-        public static final int textView3=0x7f0a000e;
-        public static final int textView4=0x7f0a000f;
-        public static final int textView5=0x7f0a0010;
-        public static final int twitterView=0x7f0a0006;
-        public static final int viberView=0x7f0a0005;
-        public static final int whatsappView=0x7f0a0004;
+        public static final int action_settings=0x7f090019;
+        public static final int app_logo=0x7f090015;
+        public static final int app_name=0x7f090016;
+        public static final int begin=0x7f090001;
+        public static final int block_item=0x7f090018;
+        public static final int button=0x7f09000a;
+        public static final int check_box=0x7f090017;
+        public static final int container=0x7f090007;
+        public static final int facebookView=0x7f090003;
+        public static final int listViewLayout=0x7f090014;
+        public static final int progressBar=0x7f090002;
+        public static final int progressView1=0x7f090000;
+        public static final int section_label=0x7f090011;
+        public static final int set_block_list=0x7f090013;
+        public static final int start_block_btn=0x7f090012;
+        public static final int statusTextView=0x7f090008;
+        public static final int textView=0x7f090009;
+        public static final int textView0=0x7f09000b;
+        public static final int textView1=0x7f09000c;
+        public static final int textView2=0x7f09000d;
+        public static final int textView3=0x7f09000e;
+        public static final int textView4=0x7f09000f;
+        public static final int textView5=0x7f090010;
+        public static final int twitterView=0x7f090006;
+        public static final int viberView=0x7f090005;
+        public static final int whatsappView=0x7f090004;
     }
     public static final class layout {
         public static final int activity_applist=0x7f030000;
@@ -82,30 +82,27 @@ public final class R {
         public static final int listview_item=0x7f030007;
     }
     public static final class menu {
-        public static final int main=0x7f090000;
-        public static final int menu_setting=0x7f090001;
-    }
-    public static final class raw {
-        public static final int warningmusic=0x7f040000;
+        public static final int main=0x7f080000;
+        public static final int menu_setting=0x7f080001;
     }
     public static final class string {
-        public static final int action_settings=0x7f070000;
-        public static final int active=0x7f070001;
-        public static final int app_list_btn=0x7f070002;
-        public static final int app_name=0x7f070003;
-        public static final int common_warning_0=0x7f070004;
-        public static final int common_warning_1=0x7f070005;
-        public static final int common_warning_2=0x7f070006;
-        public static final int common_warning_3=0x7f070007;
-        public static final int common_warning_4=0x7f070008;
-        public static final int common_warning_5=0x7f070009;
-        public static final int hello_blank_fragment=0x7f07000a;
-        public static final int hello_world=0x7f07000b;
-        public static final int not_active=0x7f07000c;
-        public static final int start_block=0x7f07000d;
-        public static final int stop_block=0x7f07000e;
-        public static final int title_activity_begin=0x7f07000f;
-        public static final int title_activity_setting=0x7f070010;
+        public static final int action_settings=0x7f060000;
+        public static final int active=0x7f060001;
+        public static final int app_list_btn=0x7f060002;
+        public static final int app_name=0x7f060003;
+        public static final int common_warning_0=0x7f060004;
+        public static final int common_warning_1=0x7f060005;
+        public static final int common_warning_2=0x7f060006;
+        public static final int common_warning_3=0x7f060007;
+        public static final int common_warning_4=0x7f060008;
+        public static final int common_warning_5=0x7f060009;
+        public static final int hello_blank_fragment=0x7f06000a;
+        public static final int hello_world=0x7f06000b;
+        public static final int not_active=0x7f06000c;
+        public static final int start_block=0x7f06000d;
+        public static final int stop_block=0x7f06000e;
+        public static final int title_activity_begin=0x7f06000f;
+        public static final int title_activity_setting=0x7f060010;
     }
     public static final class style {
         /** 
@@ -117,12 +114,12 @@ public final class R {
  API 14 theme customizations can go here. 
  API 21 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f080000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f080001;
-        public static final int NoActionBar=0x7f080002;
-        public static final int NoActionBarApiSpecific=0x7f080003;
-        public static final int WarningText=0x7f080004;
+        public static final int AppTheme=0x7f070001;
+        public static final int NoActionBar=0x7f070002;
+        public static final int NoActionBarApiSpecific=0x7f070003;
+        public static final int WarningText=0x7f070004;
     }
 }
--- app/app/src/main/java/com/antisocial/app/WarningActivity.java
+++ app/app/src/main/java/com/antisocial/app/WarningActivity.java
@@ -2,36 +2,26 @@ package com.antisocial.app;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.Handler;
 
 public class WarningActivity extends Activity {
-    MediaPlayer dMediaPlayer;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_warning);
-        dMediaPlayer= MediaPlayer.create(this,R.raw.warningmusic);
-        dMediaPlayer.setVolume(1,1);
-        dMediaPlayer.start();
+
 	}
 
 	@Override
 	public void onBackPressed() {
 
-        new Handler().postDelayed(new Runnable(){
-            @Override
-            public void run() {
-                Intent MyIntent = new Intent(Intent.ACTION_MAIN);
-                MyIntent.addCategory(Intent.CATEGORY_HOME);
-                startActivity(MyIntent);
+		Intent MyIntent = new Intent(Intent.ACTION_MAIN);
+		MyIntent.addCategory(Intent.CATEGORY_HOME);
+		startActivity(MyIntent);
 
-                finish();
+		finish();
 
-                return;
-            }
-        }, 3000);
-
+		return;
 	}
 }
