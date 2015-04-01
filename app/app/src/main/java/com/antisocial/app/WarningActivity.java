988484b3246b0f4943574d8c302a08759ca31c14
Igibek Koishybayev <koishybayev.igibek@gmail.com>
2015-04-01 16:53:13 -0400

    Update

--- app/app/src/main/java/com/antisocial/app/WarningActivity.java
+++ app/app/src/main/java/com/antisocial/app/WarningActivity.java
@@ -2,26 +2,34 @@ package com.antisocial.app;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class WarningActivity extends Activity {
-
+    MediaPlayer dMediaPlayer;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_warning);
-
+        dMediaPlayer= MediaPlayer.create(this, R.raw.warningmusic);
+        dMediaPlayer.setVolume(1,1);
+        dMediaPlayer.start();
 	}
 
 	@Override
 	public void onBackPressed() {
 
-		Intent MyIntent = new Intent(Intent.ACTION_MAIN);
-		MyIntent.addCategory(Intent.CATEGORY_HOME);
-		startActivity(MyIntent);
+        new Handler().postDelayed(new Runnable(){
+            public void run() {
+                Intent MyIntent = new Intent(Intent.ACTION_MAIN);
+                MyIntent.addCategory(Intent.CATEGORY_HOME);
+                startActivity(MyIntent);
 
-		finish();
+                finish();
 
-		return;
+                return;//execute the task
+            }
+        }, 3000);
 	}
 }
